fbe0a0a8c51ddaef8f90029a8f3239cf6be9924e
Jamshid Ganiev <jamesganiev227@gmail.com>
2023-03-15 03:10:50 -0700

    week 3. lecture 1 Exercise

---
+++ week-3/week3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd ~
+cd Desktop
+mkdir test
+cd test
+mkdir oslab
+cd oslab
+mkdir week3
+cd week3
+touch week3.sh
+
+sleep 1s
+
+echo "echo 'Hello, World!. This is an automated text added inside shell'" >> week3.sh
+
+echo "Executed successfully"
+
